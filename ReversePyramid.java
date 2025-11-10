7d1b56df0751472802088d50278383686402d973
Sunkara Chaithanya Krishna <81861417+555Chaithanya@users.noreply.github.com>
2025-11-10 08:55:57 +0530

    Create ReversePyramid.java

---
+++ ReversePyramid.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Main {
+    static void RevPyramid(int n){
+        for(int i=0; i<n; i++){
+            for(int j=0; j<i; j++){
+                System.out.print(".");
+            }
+            for(int k=0; k<2*n-2*i-1; k++){
+                System.out.print("*");
+            }
+            for(int l=0; l<i; l++){
+                System.out.print(".");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        RevPyramid(n);
+    }
+}
