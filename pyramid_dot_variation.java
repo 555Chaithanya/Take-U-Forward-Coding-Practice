b1a2615c53f3fd9b3a901755ca266adacc9735a7
Sunkara Chaithanya Krishna <81861417+555Chaithanya@users.noreply.github.com>
2025-11-10 08:54:35 +0530

    Create pyramid_dot_variation.java

---
+++ pyramid_dot_variation.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+// "static void main" must be defined in a public class.
+public class Main {
+    static void Pyramid(int n){
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n-i-1; j++){
+                System.out.print(".");
+            }
+            for(int k=0; k<2*i+1; k++){
+                System.out.print("*");
+            }
+            for(int l=0; l<n-i-1; l++){
+                System.out.print(".");
+            }
+           System.out.println(); 
+        }
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Pyramid(n);
+    }
+}
